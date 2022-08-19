b25830adca20363167e139d43f3bd8885fd648e4
Pedro Henrique <contatophss11@gmail.com>
2022-08-19 11:12:37 -0300

    incluindo melhorias no form e adicionando botao topo

---
+++ js/scripts/script.js
@@ -0,0 +1,6 @@
+
+// Fazer a página voltar ao topo quando o botão for clicado
+const btn = document.querySelector("#voltar-topo");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
